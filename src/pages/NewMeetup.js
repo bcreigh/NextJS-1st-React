02ce22ada2f111c549f3e3ac1f19cb1d6ac5f769
Bob Creigh <bcreigh@bobcreigh.com>
2021-05-04 10:24:10 -0400

    Lesson 41

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,8 +1,23 @@
+import {useHistory} from 'react-router-dom'
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm"
 
 function NewMeetupPage() {
-  function addMeetupHandler(meetupData) {
+  const history = useHistory()
 
+  function addMeetupHandler(meetupData) {
+    fetch(
+      'https://react-752c8-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+      ).then(() => {
+        history.replace('/')
+      })
   }
   return (
     <section>

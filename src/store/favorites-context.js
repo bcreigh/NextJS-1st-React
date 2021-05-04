833abc38d9817d7784f075112306f5482630770d
Bob Creigh <bcreigh@bobcreigh.com>
2021-05-04 14:33:39 -0400

    Lesson 36

--- src/store/favorites-context.js
+++ src/store/favorites-context.js
@@ -3,9 +3,12 @@ import { createContext, useState } from "react";
 const FavoritesContext = createContext({
   favorites: [],
   totalFavorites: 0,
+  addFavorite: (favoriteMeetup) => {},
+  removeFavorite: (meetupId) => {},
+  itemIsFavorite: (meetupId) => {}
 });
 
-function FavoritesContextProvider(props) {
+export function FavoritesContextProvider(props) {
 
   const [userFavorites, setUserFavorites] = useState([])
 
@@ -22,12 +25,15 @@ function FavoritesContextProvider(props) {
   }
 
   function itemIsFavoriteHandler(meetupId) {
-    return userFavorites.some(meetup => meetup.id !== meetupId)
+    return userFavorites.some(meetup => meetup.id === meetupId)
   }
 
   const context = {
     favorites: userFavorites,
-    totalFavorites: userFavorites.length
+    totalFavorites: userFavorites.length,
+    addFavorite: addFavoriteHandler,
+    removeFavorite: removeFavoriteHandler,
+    itemIsFavorite: itemIsFavoriteHandler
   }
 
   return (
@@ -36,3 +42,5 @@ function FavoritesContextProvider(props) {
     </FavoritesContext.Provider>
   );
 }
+
+export default FavoritesContext
